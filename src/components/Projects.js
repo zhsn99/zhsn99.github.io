a833e8ad5e94b82971f1c40e502a1d86cbce84ef
Zari <48201383+zhsn99@users.noreply.github.com>
2025-09-22 19:47:43 -0400

    update the project lists

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -30,7 +30,7 @@ class Projects extends React.Component {
         link: "https://github.com/slakh96/no-mans-land",
         open: "https://hdl.handle.net/10315/42858",
         image: "/assets/setup_mahsa_majid.png"
-      },
+      }
       // eeeeee: {
       //   title: "ssssss",
       //   desc:
@@ -40,33 +40,33 @@ class Projects extends React.Component {
       //   open: "https://gazijarin.github.io/Truth/",
       //   image: "/assets/setup_mahsa_majid_2.png"
       // },
-      Truth: {
-        title: "truth",
-        desc:
-          "A three.js simulation of the planet system revolving around a monolith.",
-        techStack: "JAVASCRIPT (THREE.JS)",
-        link: "https://github.com/gazijarin/truth",
-        open: "https://gazijarin.github.io/Truth/",
-        image: "/assets/truth.png"
-      },
-      "Tall Tales": {
-        title: "tall tales",
-        desc:
-          "A multi-player story-telling web game for 3-5 players. Its usage of sockets to allow for concurrent gameplay, connecting friends across the internet.",
-        techStack: "NODE.JS (SOCKET.IO), REACT.JS, MONGODB",
-        link: "https://github.com/gazijarin/TallTales",
-        open: "https://talltales.herokuapp.com/",
-        image: "/assets/talltales.png"
-      },
-      Portfolio: {
-        title: "portfolio.js",
-        desc:
-          "A small JS library that helps with clear and succinct data presentation.",
-        techStack: "NODE.JS (EXPRESS.JS)",
-        link: "https://github.com/gazijarin/Portfolio.js",
-        open: "https://afternoon-ocean-92382.herokuapp.com/",
-        image: "/assets/portfolio.png"
-      }
+      // Truth: {
+      //   title: "truth",
+      //   desc:
+      //     "A three.js simulation of the planet system revolving around a monolith.",
+      //   techStack: "JAVASCRIPT (THREE.JS)",
+      //   link: "https://github.com/gazijarin/truth",
+      //   open: "https://gazijarin.github.io/Truth/",
+      //   image: "/assets/truth.png"
+      // },
+      // "Tall Tales": {
+      //   title: "tall tales",
+      //   desc:
+      //     "A multi-player story-telling web game for 3-5 players. Its usage of sockets to allow for concurrent gameplay, connecting friends across the internet.",
+      //   techStack: "NODE.JS (SOCKET.IO), REACT.JS, MONGODB",
+      //   link: "https://github.com/gazijarin/TallTales",
+      //   open: "https://talltales.herokuapp.com/",
+      //   image: "/assets/talltales.png"
+      // },
+      // Portfolio: {
+      //   title: "portfolio.js",
+      //   desc:
+      //     "A small JS library that helps with clear and succinct data presentation.",
+      //   techStack: "NODE.JS (EXPRESS.JS)",
+      //   link: "https://github.com/gazijarin/Portfolio.js",
+      //   open: "https://afternoon-ocean-92382.herokuapp.com/",
+      //   image: "/assets/portfolio.png"
+      // }
     };
     const projects = {
       "TDSB Homework Management Interface": {
